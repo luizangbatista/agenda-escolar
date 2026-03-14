dadb7fe3affaa7a591608692de4bc3e4368d8358
luizangbatista <luizangbatista@gmail.com>
2026-03-14 14:25:31 -0300

    Update app.py

--- app.py
+++ app.py
@@ -452,19 +452,19 @@ elif st.session_state.tela == "consultar":
 
     df_mon = buscar_monitorias()
 
-    if not df_mon.empty:
-        if filtro_turma != "Todas":
+        if not df_mon.empty:
+            if filtro_turma != "Todas":
             df_mon = df_mon[df_mon["turma"] == filtro_turma]
 
-        if filtro_monitor != "Todos":
+            if filtro_monitor != "Todos":
             df_mon = df_mon[df_mon["monitor"] == filtro_monitor]
 
         df_mon = filtrar_por_mes(df_mon, filtro_mes)
         df_mon = df_mon.sort_values("data_obj", ascending=False)
 
-        if df_mon.empty:
+            if df_mon.empty:
             st.info("Nenhuma monitoria encontrada com esses filtros.")
-        else:
+            else:
             for _, linha in df_mon.iterrows():
                 st.markdown(f"**{linha['data']} — {linha['turma']}**")
                 st.write(f"**Monitor:** {linha['monitor']}")
